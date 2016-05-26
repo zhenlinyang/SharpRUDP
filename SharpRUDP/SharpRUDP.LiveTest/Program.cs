196ad3136a2f78e359dec4b91cc0b128a9479116
DARKGuy <darkguy2008@gmail.com>
2016-05-26 16:29:55 -0600

    Kinda not working, have to mix these changes with the old ones

--- SharpRUDP/SharpRUDP.LiveTest/Program.cs
+++ SharpRUDP/SharpRUDP.LiveTest/Program.cs
@@ -1,18 +1,25 @@
 ﻿using System;
+using System.Threading;
 
 namespace SharpRUDP.LiveTest
 {
     class Program
     {
+        static void Wait()
+        {
+            //Console.ReadLine();
+            Thread.Sleep(5000);
+        }
+
         static void Main(string[] args)
         {
-            new Test.Connectivity().ConnectAndDisconnect(); Console.ReadLine();
-            new Test.SmallPacketTest().SmallPacket(); Console.ReadLine();
-            new Test.MediumPacketTest().MediumPacket(); Console.ReadLine();
-            new Test.MultiPacketSmallTest().MultiPacketSmall(); Console.ReadLine();
-            new Test.MultiPacketMediumTest().MultiPacketMedium(); Console.ReadLine();
-            new Test.MultiPacketLargeTest().MultiPacketLarge(); Console.ReadLine();
-            new Test.MultiPacketExtraLargeTest().MultiPacketExtraLarge(); Console.ReadLine();
+            new Test.Connectivity().ConnectAndDisconnect(); Wait();
+            new Test.SmallPacketTest().SmallPacket(); Wait();
+            new Test.MediumPacketTest().MediumPacket(); Wait();
+            new Test.MultiPacketSmallTest().MultiPacketSmall(); Wait();
+            new Test.MultiPacketMediumTest().MultiPacketMedium(); Wait();
+            new Test.MultiPacketLargeTest().MultiPacketLarge(); Wait();
+            new Test.MultiPacketExtraLargeTest().MultiPacketExtraLarge(); Wait();
         }
     }
 }
